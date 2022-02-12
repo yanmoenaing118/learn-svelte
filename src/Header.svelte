c1058dbde8007764410be8c3d5d95d2aa1de10f4
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-02-12 13:53:27 +0630

    cmt

---
+++ src/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  import MaxWidth from "./MaxWidth.svelte";
+</script>
+
+<header>
+  <!-- <MaxWidth> -->
+  <div>
+    <h1>Hello</h1>
+  </div>
+  <!-- </MaxWidth> -->
+</header>
+
+<style>
+</style>
---
+++ src/MaxWidth.svelte
@@ -0,0 +1,8 @@
+<div />
+
+<style>
+  div {
+    max-width: 1120px;
+    margin: 0 auto;
+  }
+</style>
